8392b2c3dc724711a82413d9aa4f5d55b99fb959
Eko Juli Saputro <17070343@simasfinance.local>
2026-06-02 12:34:51 +0700

    fix(fabric): correct MSP path in deploy script

--- fabric-local/deploy-chaincode.sh
+++ fabric-local/deploy-chaincode.sh
@@ -8,12 +8,12 @@ for ORG in portorg customsorg bankorg; do
   echo "2. Meng-install chaincode di node ${ORG}..."
   docker exec -e CORE_PEER_LOCALMSPID=${ORG}MSP \
               -e CORE_PEER_ADDRESS=${ORG}peer-api.127-0-0-1.nip.io:8080 \
-              -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-${ORG}/msp \
+              -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-${ORG} \
               microfab peer lifecycle chaincode install /tmp/portchain-cc.tar.gz || true
 done
 
 echo "3. Mengambil Package ID..."
-PKG_ID=$(docker exec -e CORE_PEER_LOCALMSPID=portorgMSP -e CORE_PEER_ADDRESS=portorgpeer-api.127-0-0-1.nip.io:8080 -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-portorg/msp microfab peer lifecycle chaincode queryinstalled | grep portchain-cc_1.0 | awk '{print $3}' | tr -d ',')
+PKG_ID=$(docker exec -e CORE_PEER_LOCALMSPID=portorgMSP -e CORE_PEER_ADDRESS=portorgpeer-api.127-0-0-1.nip.io:8080 -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-portorg microfab peer lifecycle chaincode queryinstalled | grep portchain-cc_1.0 | awk '{print $3}' | tr -d ',')
 echo "Package ID adalah: $PKG_ID"
 
 if [ -z "$PKG_ID" ]; then
@@ -25,14 +25,14 @@ for ORG in portorg customsorg bankorg; do
   echo "4. Menyetujui (Approve) chaincode untuk organisasi ${ORG}..."
   docker exec -e CORE_PEER_LOCALMSPID=${ORG}MSP \
               -e CORE_PEER_ADDRESS=${ORG}peer-api.127-0-0-1.nip.io:8080 \
-              -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-${ORG}/msp \
+              -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-${ORG} \
               microfab peer lifecycle chaincode approveformyorg -o orderer-api.127-0-0-1.nip.io:8080 --channelID mychannel --name portchain-cc --version 1.0.1 --package-id $PKG_ID --sequence 1 || true
 done
 
 echo "5. Melakukan Commit chaincode ke channel (Hanya perlu sekali)..."
 docker exec -e CORE_PEER_LOCALMSPID=portorgMSP \
             -e CORE_PEER_ADDRESS=portorgpeer-api.127-0-0-1.nip.io:8080 \
-            -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-portorg/msp \
+            -e CORE_PEER_MSPCONFIGPATH=/opt/microfab/data/admin-portorg \
             microfab peer lifecycle chaincode commit -o orderer-api.127-0-0-1.nip.io:8080 --channelID mychannel --name portchain-cc --version 1.0.1 --sequence 1 --peerAddresses portorgpeer-api.127-0-0-1.nip.io:8080 --peerAddresses customsorgpeer-api.127-0-0-1.nip.io:8080 --peerAddresses bankorgpeer-api.127-0-0-1.nip.io:8080
 
 echo "6. Inisialisasi awal (jika diperlukan)..."
