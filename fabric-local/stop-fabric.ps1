3b7ac3b30dafd3763c55eb75f177b17970c57ece
Eko Juli Saputro <17070343@simasfinance.local>
2026-06-03 13:33:18 +0700

    chore: remove obsolete ps1 scripts

--- fabric-local/stop-fabric.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Menghentikan Jaringan Hyperledger Fabric Microfab..." -ForegroundColor Yellow
-
-$pwd = (Get-Location).Path
-cd $pwd
-
-docker-compose down
-
-Write-Host "Jaringan berhasil dihentikan." -ForegroundColor Green
